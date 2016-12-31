da2ff641e6127663b3b46849c8c677f8b9c9eff6
Grace Nellore <gracernellore@gmail.com>
2016-12-31 12:35:02 -0800

    hides scroll top on load, and fades in after scrolling down

--- p02-portfolio-page/action.js
+++ p02-portfolio-page/action.js
@@ -2,3 +2,16 @@
 $('.nav a').on('click', function(){
     $('.navbar-toggle').click()
 });
+
+//hides scroll-top on initial load/before any scrolling
+$('.scroll-top').hide();
+
+//shows scroll-top after scrolling down
+//http://stackoverflow.com/a/5980361
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('.scroll-top').fadeIn();
+    } else {
+        $('.scroll-top').hide();
+    }
+});
