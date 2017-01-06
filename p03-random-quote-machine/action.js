85f8d3f2e5f5226ab0735b1441780819f8afa55d
Grace Nellore <gracernellore@gmail.com>
2017-01-05 20:36:54 -0800

    api json data to update quote and author elements

--- p03-random-quote-machine/action.js
+++ p03-random-quote-machine/action.js
@@ -1,13 +1,19 @@
+var forismaticAPI = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var randomColor;
+var $html = $("html");
+var $body = $("body");
+var $quoteBtn = $("button");
+var $quote = $("#quote");
+var $author = $("#author");
+var $fontAwesome = $(".fa");
+var $foot = $(".foot");
 
 $(document).ready(function(){
 
   //changes color in response to button click
-  $("button").on("click", function(){
-    randomColor = getRandomColor();
-    $("html").css("background-color", randomColor);
-    $("body").css("background-color", randomColor);
-    $("button").css("background-color", randomColor);
+  $("button").click(function(){
+    applyRandomColor();
+    showRandomQuote();
   });
 });
 
@@ -19,4 +25,27 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+//applys color to target elements
+function applyRandomColor() {
+  randomColor = getRandomColor();
+  $html.css("background-color", randomColor);
+  $body.css("background-color", randomColor);
+  $quoteBtn.css("background-color", randomColor);
+  $quote.css("color", randomColor);
+  $author.css("color", randomColor);
+  $fontAwesome.css("color", randomColor);
+}
+
+//gets data, then clears old and appends new quote & author
+function showRandomQuote() {
+  $.getJSON(forismaticAPI, function(data){
+    //adds 5% bottom margon when quote text length is longer
+    if (data.quoteText.length > 115) {
+        $foot.css("margin-bottom", "5%");
+    }
+    $quote.html(data.quoteText);
+    $author.html(data.quoteAuthor);
+  });
 }
