4660f159f435a7f751b5249c3e83e1db5daef27d
Grace Nellore <gracernellore@gmail.com>
2017-01-17 13:25:52 -0800

    alg adv-level, validate US telephone numbers

--- a03-advanced/validatePhoneNums.js
+++ a03-advanced/validatePhoneNums.js
@@ -15,15 +15,19 @@ Description:
 For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
 
 Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+https://regex101.com/ (more beginner-friendly, but not taking in $ correctly)
+http://www.regexpal.com/ (less training wheels, but taking $ end correctly)
 */
 
 function telephoneCheck(str) {
-  var countDigits = str.match(/\d/g).length;
-  //first rule out obvious invalid numbers
-  if (str.match(/[^0-9()-\s]/) || //characters outside [0-9, (, ), -]
-      countDigits===11 && str.charAt(0)!=="1" || //invalid country code
-      countDigits>11 || countDigits<10 //length>11 or <9
-  ) return false;
-
-  return true;
+  //^1? : starts "^" with "1" maybe "?"
+  //[\s-]? : whitespace "\s" or "-" maybe "?"
+  //((\d{3})|(\((\d{3})\))) : either 3 digits or 3 digits enclosed in escaped "\" ()
+  //\d{3} : 3 digits
+  //\d{4}$ : 4 digits ending "$"
+  var re = /^1?[\s-]?((\d{3})|(\((\d{3})\)))[\s-]?\d{3}[\s-]?\d{4}$/;
+  return re.test(str);
 }
+
+//wanted to declare parts separately then concat together
+//but losing escape characters, and consequently regex failing
