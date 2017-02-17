b178224738db7b17929a070d5b2a2068c283bedf
Grace Nellore <gracernellore@gmail.com>
2017-02-17 10:05:15 -0800

    added props to constructor, https://facebook.github.io/react/docs/react-component.html#constructor

--- p11-markdown-previewer/app.js
+++ p11-markdown-previewer/app.js
@@ -1,8 +1,8 @@
 var INITIAL_VALUE = "## FCC - Markdown Previewer\n\nSee **Wikipedia** Markdown article [_here_](http://en.wikipedia.org/wiki/Markdown).\n\n### Quotes:\n\n>  “A goal without a plan is just a wish.” -Antoine de Saint-Exupéry\n\n### Code:\n\n```\nalert(\"Let\'s go 2017!\");\n```\n\n### Bulleted List:\n\n- mango\n- pita bread\n    - raw honey\n    - peach marmalade\n- wooden basket\n\n### Numbered List:\n\n1. ziplines\n2. projects\n    - get creative\n    - get busy\n3. start search\n\n### Task List:\n\n- [ ] G\n- [ ] O \n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .";
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       markdown: INITIAL_VALUE
     };
