bc078967e62ce89dcedd6020389a3a5f6caaf489
Grace Nellore <gracernellore@gmail.com>
2017-03-02 09:32:21 -0800

    es6-ified

--- p01-tribute-page/action.js
+++ p01-tribute-page/action.js
@@ -25,7 +25,7 @@ const TEXT = {
 
 /* COMPONENTS */
 
-function Header() {
+const Header = () => {
   console.log("INFO:\tFUNC - Header()");
   return (
     <div id="header">
@@ -35,7 +35,7 @@ function Header() {
   );
 }
 
-function Picture() {
+const Picture = () => {
   console.log("INFO:\tFUNC - Picture()");
   return (
     <div id="picture" className="text-center">
@@ -47,13 +47,13 @@ function Picture() {
   );
 }
 
-function Timeline() {
+const Timeline = () => {
   console.log("INFO:\tFUNC - Timeline()");
   return (
     <div id="timeline" className="row">
       <div className="col-md-8 col-md-offset-2">
         <h3>{TEXT.timeline}</h3>
-        //the fun part, getting timeline items from source url
+        {/*the fun part, getting timeline items from source url*/}
         <TimelineItems />
         <p id="timeline-source">*Click <a href={SRC.timeline} target="_blank">here</a> for timeline source.</p>
         <div>
@@ -66,72 +66,74 @@ function Timeline() {
   );
 }
 
-var TimelineItems = React.createClass({
-    //lifecycle, https://medium.com/react-ecosystem/react-components-lifecycle-ce09239010df#.ckstiuoqa
-    getInitialState: function() {
-      return {
-          data: [],
-          fetching: false,
-          error: null
-      };
-    },
+class TimelineItems extends React.Component {
+  //lifecycle, https://medium.com/react-ecosystem/react-components-lifecycle-ce09239010df#.ckstiuoqa
+  constructor(props) {
+    super(props);
 
-    render: function() {
-      console.log("INFO:\tFUNC - TimelineItems() => RENDERING");
-      //returns placeholder if still loading or data yet null
-      if (this.props.fetching || this.state.data.length===0) {
-          return <div className='loading'>Loading...</div>;
-      }
-      //returns error message
-      if (this.props.error) {
-          return <div className='error'>{this.state.error.message}</div>;
-      }
-      //returns content, will be continuously rendered with new/updated data
-      return (
-        <div className='loaded'>
-          <ul>
-            {
-              //using map function to loop through each state data array and render UI
-              this.state.data.map(function(item) {
-                return (
-                  <li key={item[0]}>
-                    <span className="tyear">{item[1]}</span> -&nbsp;{item[2]}
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      );
-    },
-
-    componentDidMount: function() {
-      console.log("INFO:\tFUNC - TimelineItems() => MOUNTING");
-      this.setState({fetching: true});
-      $.get('http://localhost:1337/www.pbs.org/wgbh/americanexperience/features/timeline/eleanor/').done(function(data) {
-        //creating new html element to store source as html
-        let source = document.createElement("html");
-        source.innerHTML = data;
-        //now able to search within html-converted content for className
-        let items = $(source).find('.timeline_item');
-        //looping through each chunk for item year and content
-        //also fixes previous issue of misalignment between year and content when accessing purely on index
-        for (var i=0; i<items.length; i++) {
-          let sourceChunk = document.createElement('div');
-          sourceChunk.innerHTML = items[i].innerHTML;
-          let date = $(sourceChunk).find('.timeline_date').text();
-          let year = date.substr(date.length-4);
-          let content = $(sourceChunk).find('.timeline_item_content').text();
-          //mutate state array, http://stackoverflow.com/a/26254086
-          let data = this.state.data;
-          this.state.data.push([i, year, content]);
-          this.setState({data: data});
-        }
-      }.bind(this)); //not fully understanding this part still, but it is important
+    this.state = {
+      data: [],
+      fetching: false,
+      error: null
     }
-});
+  }
 
-function Footer() {
+  render() {
+    console.log("INFO:\tFUNC - TimelineItems() => RENDERING");
+    //returns placeholder if still loading or data yet null
+    if (this.props.fetching || this.state.data.length===0) {
+        return <div className='loading'>Loading...</div>;
+    }
+    //returns error message
+    if (this.props.error) {
+        return <div className='error'>{this.state.error.message}</div>;
+    }
+    //returns content, will be continuously rendered with new/updated data
+    return (
+      <div className='loaded'>
+        <ul>
+          {
+            //using map function to loop through each state data array and render UI
+            this.state.data.map(function(item) {
+              return (
+                <li key={item[0]}>
+                  <span className="tyear">{item[1]}</span> -&nbsp;{item[2]}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("INFO:\tFUNC - TimelineItems() => MOUNTING");
+    this.setState({fetching: true});
+    $.get('http://localhost:1337/www.pbs.org/wgbh/americanexperience/features/timeline/eleanor/').done(function(data) {
+      //creating new html element to store source as html
+      let source = document.createElement("html");
+      source.innerHTML = data;
+      //now able to search within html-converted content for className
+      let items = $(source).find('.timeline_item');
+      //looping through each chunk for item year and content
+      //also fixes previous issue of misalignment between year and content when accessing purely on index
+      for (var i=0; i<items.length; i++) {
+        let sourceChunk = document.createElement('div');
+        sourceChunk.innerHTML = items[i].innerHTML;
+        let date = $(sourceChunk).find('.timeline_date').text();
+        let year = date.substr(date.length-4);
+        let content = $(sourceChunk).find('.timeline_item_content').text();
+        //mutate state array, http://stackoverflow.com/a/26254086
+        let data = this.state.data;
+        this.state.data.push([i, year, content]);
+        this.setState({data: data});
+      }
+    }.bind(this)); //not fully understanding this part still, but it is important
+  }
+}
+
+const Footer = () => {
   console.log("INFO:\tFUNC - Footer()");
   return (
     <div>
@@ -145,7 +147,7 @@ function Footer() {
 
 /* Putting it all together */
 
-function TributePage() {
+const TributePage = () => {
   console.log("INFO:\tFUNC - TributePage()");
   return (
     <div id="main">
