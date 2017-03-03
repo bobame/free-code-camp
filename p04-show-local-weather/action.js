85662effa53e2cc8626212a2ddbdae759d748324
Grace Nellore <gracernellore@gmail.com>
2017-03-03 14:47:33 -0800

    geolocation position and weather api

--- p04-show-local-weather/action.js
+++ p04-show-local-weather/action.js
@@ -1,73 +1,104 @@
 /* CLI => python -m SimpleHTTPServer */
 
-/* HELPERS */
-const getFahrenheit = () => {
-  console.log("INFO:\tHELPER - getFahrenheit()");
-}
-
-const getPosition = () => {
-  console.log("INFO:\tHELPER - getPosition()");
-  //checking for geolocation, https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation#The_geolocation_object
-  if ("geolocation" in navigator) {
-    console.log("TRUE - geolocation in navigator");
-  } else {
-    console.log("FALSE - geolocation not in navigator");
-  }
-}
-
-/* COMPONENTS */
 const Header = () => {
-  console.log("INFO:\tCOMPONENT - <Header />");
+  console.log('INFO:\tCOMPONENT - <Header />');
   return(
-    <div className="container-fluid header">
+    <div className='container-fluid header'>
       <h1>Free Code Camp</h1>
       <h3>Local Weather App</h3>
     </div>
   );
 }
 
+const Temperature = () => {
+  return(
+    <div className="row text-center">
+      <span id="weather-icon">
+        <i className="fa fa-thermometer-three-quarters" />
+      </span>
+      <button id="degrees">
+        00&#176; F
+      </button>
+    </div>
+  );
+}
+
+const WeatherData = () => {
+  return(
+    <div className='row boxes'>
+      <div className='col'>
+        <span id='location'>
+          Your location
+        </span>
+      </div>
+      <div className='col'>
+        <span id="sky">
+          Sky condition
+        </span>
+      </div>
+      <div className='col'>
+        <span id='wind'>
+          Wind Speed m/s
+        </span>
+      </div>
+    </div>
+  );
+}
+
 class LocalWeather extends React.Component {
   constructor(props) {
-    console.log("INFO:\tCOMPONENT - <LocalWeather />");
+    console.log('INFO:\tCOMPONENT - <LocalWeather />');
     super(props);
 
     this.state = {
       data: null
     };
-
   }
 
   render() {
-    console.log("INFO:\tLIFECYCLE - LocalWeather.render()");
+    console.log('INFO:\tLIFECYCLE - LocalWeather.render()');
+    // Dispay message indication loading weather data
+    if (!this.state.data) {
+      return (
+        <div className='mssg-grabbing'>Grabbing weather data...</div>
+      );
+    }
+    // Display weather data
     return (
-      null
+      <div className="container-fluid content">
+        <Temperature />
+        <WeatherData />
+      </div>
     );
   }
 
   componentDidMount() {
-    console.log("INFO:\tLIFECYCLE - LocalWeather.componentDidMount()");
+    console.log('INFO:\tLIFECYCLE - LocalWeather.componentDidMount()');
+
+    // setState undefined in geolocation callback, http://stackoverflow.com/a/38302892
+    let self = this;
     const URL = 'http://api.openweathermap.org/data/2.5/weather?';
-    //lat=35&lon=139&appid=987bdab9b6f926dbb6de05bf67491e93";
-    getPosition();
-    let LAT = 35;
-    let LONG = 139;
-    let API_KEY = '987bdab9b6f926dbb6de05bf67491e93';
-    let API_URL = `${URL}lat=${LAT}&lon=${LONG}&appid=${API_KEY}`;
+    const API_KEY = '987bdab9b6f926dbb6de05bf67491e93';
+    // Checking for geolocation, https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation#The_geolocation_object
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let API_URL = `${URL}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}`;
 
-    console.log("INFO:\tAPI_URL", API_URL);
-    $.getJSON(API_URL).done((response) => {
-      console.log(response);
-
-      this.setState({
-        data: response
+        $.getJSON(API_URL).done((response) => {
+          console.log('INFO:\tAPI response - ', response);
+          self.setState({ data: response });
+        }.bind(this));
       });
-    }.bind(this));
+    } else {
+      alert('ERROR:\tNo geolocation in current navigator.');
+    }
   }
 }
 
+// Putting it all together
 const App = () => {
   return(
-    <div className="container-fluid text-center">
+    <div className='container-fluid text-center'>
       <Header />
       <LocalWeather />
     </div>
