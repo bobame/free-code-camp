b08e51d05871bcc30c7cc848b22f8438362f4e86
Grace Nellore <gracernellore@gmail.com>
2017-04-17 23:12:25 -0700

    Whitespace text nodes cannot appear as a child of <tbody>.

--- p12-camper-leaderboard/app.js
+++ p12-camper-leaderboard/app.js
@@ -24,18 +24,44 @@ const FillerContent = () => {
   );
 }
 
-const Leaderboard = () => {
+const Leaderboard = (props) => {
   return (
     <div className='container'>
-      
+      <table className='table table-hover'>
+        <thead>
+          <tr id='th-main'>
+            <th colSpan={4} className='text-center'>Camper Leaderboard</th>
+          </tr>
+          <tr id='th-sub'>
+            <th className='text-center'>#</th>
+            <th className='text-center'>Camper Name</th>
+            <th className='text-center'>Points (last 30 days)&nbsp;
+              <button
+                onClick={props.sortRecent}
+                id="sort-recent">
+                <i className="fa fa-sort-desc" aria-hidden="true"></i>
+              </button>
+            </th>
+            <th className='text-center'>Points (all time)&nbsp;
+              <button
+                onClick={props.sortAll}
+                id="sort-all">
+                <i className="fa fa-sort-desc" aria-hidden="true"></i>
+              </button>
+            </th>
+          </tr>
+        </thead>
+        {
+          (props.leaders)? props.displayData() : null
+        }
+      </table>
     </div>
   );
 }
 
-
 const Footer = () => {
   return (
-    <div className='navbar navbar-bottom' id='footer'>
+    <div className='navbar navbar-fixed-bottom' id='footer'>
       <div id='creds'>
         by <a href={Data.url_fcc} target='_blank'>@havefuncoding</a>
       </div>
@@ -49,13 +75,52 @@ class App extends React.Component {
     this.state = {
       data: []
     }
+    this.sortRecent = this.sortRecent.bind(this);
+    this.sortAll = this.sortAll.bind(this);
+    this.displayData = this.displayData.bind(this);
+  }
+
+  sortRecent() {
+    const sourceRecent = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+    console.log('clicked button to sort by recent');
+    $.getJSON(sourceRecent).done((response) => {
+      this.setState({
+        data: response.map((x, i) => {
+          `<tr>
+            <td>${i}</td>
+            <td>${x.img}${x.username}</td>
+            <td>${x.recent}</td>
+            <td>${x.alltime}</td>
+          </tr>`
+        }).join('\n')
+      });
+    }.bind(this));
+  }
+
+  sortAll() {
+    console.log('clicked button to sort by all');
+  }
+
+  displayData() {
+    console.log('need to display data');
+    return (
+      <tbody>
+        {
+          (document.querySelector('tbody'))? document.querySelector('tbody').innerHTML = this.state.data: null
+        }
+      </tbody>
+    );
   }
 
   render() {
     return (
       <div>
         <Header />
-        <Leaderboard />
+        <Leaderboard
+          sortRecent={this.sortRecent}
+          sortAll={this.sortAll}
+          leaders={this.state.data}
+          displayData={this.displayData} />
         <Footer />
       </div>
     );
